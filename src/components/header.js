5c47e9e61ee83e652237e5b23f72f26a04e3f64d
tylertran349 <tylertran349@gmail.com>
2023-09-11 18:51:50 -0700

    Add instructions

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,9 @@ export function Header(props) {
     return (
         <div id="header">
             <img src={image} alt="NFL Logo" />
-            <span>Memory Game</span>
+            <span id="title">Memory Game</span>
             <ScoreCounterContainer score={props.score} highScore={props.highScore} />
+            <span id="instructions">Get points by clicking on an image but do not click on an image twice in a row.</span>
         </div>
     )
 }
